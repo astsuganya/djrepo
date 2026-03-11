3b32b9fc916fd78051018afdbac0451375585139
astsuganya <141382963+astsuganya@users.noreply.github.com>
2026-03-11 19:53:18 +0700

    Create Admin #2

--- mysite/asgi.py
+++ mysite/asgi.py
@@ -14,3 +14,21 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = get_asgi_application()
+
+
+try:
+    from django.contrib.auth.models import User
+    username = 'admin'
+    password = 'Test123456'
+    email = 'admin@example.com'
+
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username, email, password)
+        print(f"✅ Superuser '{username}' created!")
+    else:
+        user = User.objects.get(username=username)
+        user.set_password(password)
+        user.save()
+        print(f"🔄 Password for '{username}' updated!")
+except Exception as e:
+    print(f"❌ Error creating superuser: {e}")
